7a4b3b09a83b48dfc483a8c7900dd5fc17f525bc
rafiki <mark@rafiki>
2015-06-05 10:30:06 +0200

    infrastructure for py.test tests

---
+++ setup.py
@@ -0,0 +1,56 @@
+
+"""
+	Install svSite and dependencies.
+
+	Tests based on https://pytest.org/latest/goodpractises.html .
+"""
+
+from distutils.core import setup, Command
+
+
+class PyTest(Command):
+	user_options = []
+
+	def initialize_options(self):
+		pass
+
+	def finalize_options(self):
+		pass
+
+	def run(self):
+		from subprocess import call
+		from sys import executable
+		errno = call([executable, 'tests/runtests.py', 'tests'])
+		raise SystemExit(errno)
+
+
+setup(
+	name='svsite',
+	version='0.1.0',
+	packages=['', 'svsite'],
+	package_dir={'': 'source'},
+	package_data={
+		'docs': 'docs',
+		'static': 'static',
+	},
+	test_suite='tests',
+	url='https://github.com/mverleg/svsite',
+	license='MIT License',
+	author='mark, ...',
+	author_email='contact me on github',
+	description='reusable basic for an association website',
+	install_requires=[
+		'django',
+	],
+	classifiers=[
+		'Development Status :: 2 - Pre-Alpha',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3.4',
+		'Framework :: Django :: 1.8',
+		'Environment :: Web Environment',
+	],
+	cmdclass = {'test': PyTest},
+)
+
+
