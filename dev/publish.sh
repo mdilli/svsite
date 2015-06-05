b15f575080f2e62fdc5685465e261dccc48275bd
rafiki <mark@rafiki>
2015-06-05 11:38:41 +0200

    steps for publishing as a bash script

---
+++ dev/publish.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# run tests with py.test
+py.test tests || exit 10
+
+# check style with pylint, exit for any problem except convention messages
+pylint source
+if [ "$?" -ne 16 ]; then exit 20; fi
+
+# create a file to run tests without needing py.test (or plugins)
+py.test --genscript=tests/runtests.py || exit 30
+
+# create sphix documentation
+cd docs && make dirhtml && cd .. || exit 40
+
+# the user should not forget to update the version number, and create a tag in git
+printf "\nThe current version is: %s\n" "$(cat 'dev/VERSION')"
+printf "Do not forget to increment it, and add a git tag\n"
+
+
