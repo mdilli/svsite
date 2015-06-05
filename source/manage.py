24227ce3c5cb75d68ed09fdcf3592b0d5d223ca1
rafiki <mark@rafiki>
2015-06-05 20:55:19 +0200

    docstrings and local settings

--- source/manage.py
+++ source/manage.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
-import os
-import sys
+
+"""
+	Django default file that runs the server and other commands.
+"""
+
+from os import environ
+from sys import argv
+
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "svsite.settings")
+	environ.setdefault("DJANGO_SETTINGS_MODULE", "svsite.settings")
+	from django.core.management import execute_from_command_line
+	execute_from_command_line(argv)
 
-    from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
--- source/svsite/settings.py
+++ source/svsite/settings.py
@@ -1,8 +1,14 @@
 
+"""
+	General settings for the svSite project.
+
+	Location specific settings can be set in settings_local.py .
+"""
+
 from os.path import dirname, abspath, join
 
-BASE_DIR = dirname(dirname(abspath(__file__)))
 
+BASE_DIR = dirname(dirname(dirname(abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -18,41 +24,41 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'svsite.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'svsite.wsgi.application'
@@ -62,10 +68,10 @@ WSGI_APPLICATION = 'svsite.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': join(BASE_DIR, 'data', 'default.sqlite3'),
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.sqlite3',
+		'NAME': join(BASE_DIR, 'data', 'default.sqlite3'),
+	}
 }
 
 
@@ -87,3 +93,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+	from settings_local import *
+except ImportError:
+	pth = join(BASE_DIR, 'source', 'svsite', 'settings_local.py')
+	print('creating local settings file "{0:s}"'.format(pth))
+	with open(pth, 'w+') as fh:
+		fh.write('\n"""\n\tLocal settings for this specific instance of svSite (e.g. passwords, absolute paths, ...).\n"""\n')
+
+
