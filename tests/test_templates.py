b4b2e0b7b3ef663a10e556124d54082b42b835e3
rafiki <mark@rafiki>
2015-06-10 11:43:19 +0200

    properly indent base template and write a test to check templates for syntax errors (just django_extensions with stdout-hack)

---
+++ tests/test_templates.py
@@ -0,0 +1,16 @@
+
+from os import devnull
+from django.core.management import call_command
+import sys
+
+
+def test_templates_valid():
+	"""
+		Check templates based on django-extensions' validate_templates. Some magic with stdout because the stdout argument gets ignored.
+	"""
+	null = open(devnull, 'a')
+	stdout_backup, sys.stdout = sys.stdout, null
+	call_command('validate_templates', '--break', '--check-urls', interactive = False, stdout = null)
+	sys.stdout = stdout_backup
+
+
