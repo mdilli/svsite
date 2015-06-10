a0a24eb792a94649106c6d74f7ec01c363d94128
rafiki <mark@rafiki>
2015-06-10 21:45:43 +0200

    added django-model-utils and use it for event; also update user admin

--- source/activity/models.py
+++ source/activity/models.py
@@ -3,14 +3,15 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils.text import slugify
 from django.utils.translation import ugettext_lazy as _
+from model_utils.models import TimeFramedModel
 
 
-class Activity(models.Model):
+class Activity(TimeFramedModel):
 
 	name = models.CharField(max_length = 64)
 	description = models.TextField(blank = True)
-	start = models.DateTimeField(blank = True, null = True, help_text = 'When does the event start?')
-	end = models.DateTimeField(blank = True, null = True, help_text = 'When does the event end?')
+	#start = models.DateTimeField(blank = True, null = True, help_text = 'When does the event start?')
+	#end = models.DateTimeField(blank = True, null = True, help_text = 'When does the event end?')
 	member_cost = models.DecimalField(max_digits = 8, decimal_places = 2, help_text = 'Cost for attending this event')
 	outsider_allowed = models.BooleanField(default = True, help_text = 'Are non-members welcome at this event?')
 	outsider_cost = models.DecimalField(max_digits = 8, decimal_places = 2, help_text = 'Cost for attending this event if you are not a member')
--- source/member/admin.py
+++ source/member/admin.py
@@ -1,4 +1,9 @@
 
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import svUser
 
-# Register your models here.
+
+admin.site.register(svUser, UserAdmin)
+
+
