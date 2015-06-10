817f648fc26a41d4d5efb5ea1dcf694d927a9a68
rafiki <mark@rafiki>
2015-06-11 00:18:12 +0200

    make backups work and have lazy-loaded special permissions for it (can't be done in migrate)

--- source/member/models.py
+++ source/member/models.py
@@ -3,10 +3,14 @@ from django.contrib.auth.models import AbstractUser
 from django.core.urlresolvers import reverse
 from django.utils.text import slugify
 from member.functions import email_to_name
+from django.utils.translation import ugettext_lazy as _
 
 
 class svUser(AbstractUser):
 
+	class Meta:
+		verbose_name = _('Member')
+		verbose_name_plural = _('Members')
 
 	def get_absolute_url(self):
 		return reverse('member_profile', kwargs = {'pk': self.pk, 'label': slugify(self.get_full_name())})
---
+++ source/svsite/functions.py
@@ -0,0 +1,29 @@
+
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.db.models.loading import get_model
+
+
+def lazy_create_permission(codename):
+	"""
+		Have to lazy-create this, since it can't reasonably be created during migration since ContentType doesn't exist.
+
+		https://stackoverflow.com/questions/29296757/django-data-migrate-permissions
+	"""
+	app_label, model = settings.AUTH_USER_MODEL.split('.')
+	UserType = ContentType.objects.get(app_label = app_label, model = model)
+	Permission = get_model('auth', 'Permission')
+	try:
+		perm = Permission.objects.get(
+			codename = codename,
+			content_type = UserType
+		)
+	except Permission.DoesNotExist:
+		perm = Permission.objects.create(
+			codename = codename,
+			content_type = UserType
+		)
+		perm.save()
+	return perm
+
+
