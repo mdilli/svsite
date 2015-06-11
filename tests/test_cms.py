16400c68e0bf4711ec5124e4e4e9557257b9e07d
rafiki <mark@rafiki>
2015-06-11 23:46:58 +0200

    simple test based on `cms check` management command

--- tests/test_cms.py
+++ tests/test_cms.py
@@ -4,15 +4,15 @@ from django.core.management import call_command
 import sys
 
 
-def test_templates_valid():
+def test_cms_check():
 	"""
-		Check templates based on django-extensions' validate_templates. Some magic with stdout because the stdout argument gets ignored.
+		Test django-cms with it's build it command `cms check`.
 
 		The `call_command` will raise an error when it fails, no need for asserts.
 	"""
 	null = open(devnull, 'a')
 	stdout_backup, sys.stdout = sys.stdout, null
-	call_command('validate_templates', '--break', '--check-urls', interactive = False, stdout = null)
+	call_command('cms', 'check', interactive = False, stdout = null)
 	sys.stdout = stdout_backup
 
 
