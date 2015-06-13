21adc0b4a0582a1b86524815a41d4b20d2574608
rafiki <mark@rafiki>
2015-06-13 21:34:37 +0200

    moved census into member app

--- source/member/admin.py
+++ source/member/admin.py
@@ -1,9 +1,24 @@
 
+"""
+	Create a second admin website for members, and register member models.
+
+	Second admin: https://stackoverflow.com/questions/3206856/how-to-have-2-different-admin-sites-in-a-django-project
+"""
+
+from django.contrib.admin.sites import AdminSite
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import svUser
 
 
+class CensusAdminSite(AdminSite):
+	pass
+
+
+census_admin = CensusAdminSite()
+
+
+census_admin.register(svUser, UserAdmin)
 admin.site.register(svUser, UserAdmin)
 
 
--- source/svsite/urls.py
+++ source/svsite/urls.py
@@ -1,19 +1,3 @@
-"""
-svsite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-	https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-	1. Add an import:  from my_app import views
-	2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-	1. Add an import:  from other_app.views import Home
-	2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-	1. Add an import:  from blog import urls as blog_urls
-	2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
 
 from django.conf import settings
 from django.conf.urls import include, url, patterns
@@ -24,10 +8,10 @@ from django.http import HttpResponse
 from django.views import static
 import grappelli.urls
 import allauth.urls
-import census.urls
-import member.urls
-import activity.urls
 import cms.urls
+import member.census_urls
+import member.user_urls
+import activity.urls
 from svsite.backups import download_database, upload_database
 
 try:
@@ -44,8 +28,8 @@ urlpatterns = i18n_patterns('',
 	url(r'^admin/restore/', upload_database, name = 'restore'),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^user/', include(allauth.urls)),
-	url(r'^census/', include(census.urls)),
-	url(r'^user/', include(member.urls)),
+	url(r'^census/', include(member.census_urls)),
+	url(r'^user/', include(member.user_urls)),
 	url(r'^event/', include(activity.urls)),
 	url(r'^c/', include(cms.urls)),
 	#todo: if cms keeps it's prefix, make a redirect for leftover urls to that prefix
