ffcd01d676f6b62e3f503e9a34e4cb1b7380c23e
rafiki <mark@rafiki>
2016-02-19 12:33:34 +0100

    small fix that should have been in v0.5

--- source/searcher/urls.py
+++ source/searcher/urls.py
@@ -6,7 +6,7 @@ from searcher.views import autocomplete, search
 
 
 urlpatterns = (
-	url(r'^$', search),  #todo: tmp
+	url(r'^$', search, name='search'),  #todo: tmp
 	# url(r'^old/$', SearchView(template='search.html', form_class=SearchForm), name='search'),
 	url(r'^complete/$', autocomplete, name='autocomplete'),
 )
