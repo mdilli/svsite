141fa2480bde745d06a5b82b0c5f322ff334282b
mark <mark@rafiki>
2016-06-06 12:21:24 +0200

    Revert "undo 2c6b8aed75c991f98cd83841b21094c1aac5db3e on master only"
    This reverts commit 7b57e8e1ee889dd4b659e5ea882d2be50f2d2779.

--- source/urls.py
+++ source/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 import filer.urls
 import cms.urls
+from django.shortcuts import redirect
+
 import badges.urls
 import allauth.urls
 import member.member_urls, member.team_urls
@@ -16,12 +18,14 @@ import svfinance.urls
 import intapi.urls
 from base.admin import superuser_admin, census_admin
 from base.views import playground
+from member.views import member_setup_info
 
 
 urlpatterns = i18n_patterns(
 	url(r'^€/', include(svfinance.urls)),
 	url(r'^\$playground', playground),
-	url(r'^member/', include(member.member_urls)),
+	# url(r'^member/', include(member.member_urls)),
+	url(r'^member_setup_info/$', member_setup_info, name='member_setup_info'),
 	url(r'^cie/', include(member.team_urls)),
 	url(r'^account/', include(allauth.urls)),
 	url(r'^badge/', include(badges.urls)),
@@ -32,6 +36,7 @@ urlpatterns = i18n_patterns(
 	url(r'^\$members/', include(census_admin.urls)),
 	url(r'^filer/', include(filer.urls)),   # django filer canonical urls (ones that don't change if you upload a new version)
 	url(r'^', include(base.urls)),
+	url(r'^~/$', lambda request: redirect('/'), name='home'),  #todo: improve if possible (haven't found a way, name on include doesn't work)
 	url(r'^', include(cms.urls)),
 ) + \
 static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
